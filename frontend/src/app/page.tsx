c4ef36af47cf969fca6dd43e1dc29afd12cd177f
Sahil Chabra <sahilchabra1509@gmail.com>
2024-10-24 22:26:02 +0530

    Hero-section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
+import { HeroSection } from "@/components/hero-section";
 import { NavbarHome } from "@/components/navbar";
 import { div } from "framer-motion/client";
-
 
 export default function Home() {
   return (
     <div>
-
       <NavbarHome />
+      <HeroSection />
     </div>
   );
 }
